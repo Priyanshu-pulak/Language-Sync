53c67b99e98571ced02a6a39934a8d10d00ec9f3
Priyanshu Pulak <priyanshu.pulak11@gmail.com>
2025-11-26 23:27:32 +0530

    Refactor: move routes from server.js into controllers and routes folders

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,25 +1,16 @@
 import express from "express";
-import dotenv from "dotenv";
+import "dotenv/config";
 
-dotenv.config();
+import authRoutes from "./routes/auth_route.js";
 
 const app = express();
 const PORT = process.env.PORT
+
 app.get("/", (req, res) =>{
     res.send("Hello from backend");
 });
 
-app.get("/api/auth/signup", (req, res) =>{
-    res.send("Signup route");
-});
-
-app.get("/api/auth/login", (req, res) =>{
-    res.send("Login route");
-})
-
-app.get("/api/auth/logout", (req, res) =>{
-    res.send("Logout route");
-});
-app.listen(PORT, () =>{
+app.use("/api/auth", authRoutes);
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
