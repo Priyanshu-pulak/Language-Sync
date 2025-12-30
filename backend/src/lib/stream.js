c5307c67c24e7a5ca7c4ecd8c16887930d704cbc
Priyanshu Pulak <priyanshu.pulak11@gmail.com>
2025-12-30 14:05:03 +0530

    Add comment for revision

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -8,16 +8,14 @@ if (!apiKey || !apiSecret) {
   console.error("Stream API key or Secret is missing");
 }
 
-// with the help of apikey and apisecret we can communicate 
-// with stream this is same as we were creating a connectDB in db.js
+// with the help of apikey and apisecret we can communicate with stream. This is same as we were creating a connectDB in db.js
 const streamClient = StreamChat.getInstance(apiKey, apiSecret);
 
+// upsertusers is a function in stream by which if user is not in database of stream then create it else update it
 export const upsertStreamUser = async (userData) => {
     try
     {
-        // upsertusers is a function in stream which by which if user is not in database of stream the create it else update it
         await streamClient.upsertUsers([userData]);
-
         return userData;
     }
     catch (error)
